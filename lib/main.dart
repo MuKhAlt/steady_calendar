92c9147090aeb484edf947bcb24a4763bc4e3a9e
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-09-25 21:20:48 +0100

    created App widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container());
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
